887679cd3979dab648f8bfa9af00a36fffe48d76
Leo Goubard <leogoubard@macbook-pro-de-leo.home>
2023-04-23 20:52:37 +0200

    :construction: got a ancient version of react-pro-sidebar

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { Box, IconButton, useTheme } from '@mui/material';
+import { ColorModeContext, tokens } from '../../theme';
+import InputBase from '@mui/material/InputBase';
+import { LightModeOutlined, NotificationsOutlined, SettingsOutlined, PersonOutlined, DarkModeOutlined, Search } from '@mui/icons-material';
 
 function Topbar() {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
   return (
-    <div>Topbar</div>
+    <Box display="flex" justifyContent= "space-between" p={2}>
+      <Box display="flex" backgroundColor={colors.primary[400]} borderRadius="3px">
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+          <IconButton type='button' sx={{ p: 1 }}>
+              <Search />
+          </IconButton>
+      </Box>
+      <Box display="flex">
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === 'dark' ?  (
+            <DarkModeOutlined />
+          ) : (
+            <LightModeOutlined />
+          )}
+        </IconButton>
+        <IconButton>
+          <NotificationsOutlined />
+        </IconButton>
+        <IconButton>
+        
+        </IconButton>
+        <IconButton>
+        
+        </IconButton>
+      </Box>
+    </Box>
   )
 }
 
