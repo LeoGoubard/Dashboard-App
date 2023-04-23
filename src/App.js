ad7d7700a9db4c895c12d85293adcb3b707eda7b
Leo Goubard <leogoubard@macbook-pro-de-leo.home>
2023-04-23 21:02:14 +0200

    :construction: barChart done

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import { Route, Routes } from 'react-router-dom';
 import Topbar from './scenes/global/Topbar';
 import Sidebar from './scenes/global/Sidebar';
-/* import Dashboard from './scenes/dashboard';
-import Team from './scenes/team';
+import Dashboard from './scenes/dashboard';
+/*import Team from './scenes/team';
 import Invoices from './scenes/invoices';
 import Contacts from './scenes/contacts';
 import Bar from './scenes/bar';
@@ -28,8 +28,8 @@ function App() {
             <main className="content">
               <Topbar />
               <Routes>
-                {/* <Route path="/" element={<Dashboard/>} />
-                <Route path="/team" element={<Team/>} />
+                <Route path="/" element={<Dashboard/>} />
+                {/* <Route path="/team" element={<Team/>} />
                 <Route path="/invoices" element={<Invoices/>} />
                 <Route path="/contacts" element={<Contacts/>} />
                 <Route path="/form" element={<Form/>} />
--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,25 +1,39 @@
-import React, { useContext } from 'react';
-import { Box, IconButton, useTheme } from '@mui/material';
-import { ColorModeContext, tokens } from '../../theme';
-import InputBase from '@mui/material/InputBase';
-import { LightModeOutlined, NotificationsOutlined, SettingsOutlined, PersonOutlined, DarkModeOutlined, Search } from '@mui/icons-material';
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material/InputBase";
+import {
+  LightModeOutlined,
+  DarkModeOutlined,
+  NotificationsOutlined,
+  SettingsOutlined,
+  PersonOutlined,
+  Search,
+} from "@mui/icons-material";
 
-function Topbar() {
+const Topbar = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const colorMode = useContext(ColorModeContext);
 
   return (
-    <Box display="flex" justifyContent= "space-between" p={2}>
-      <Box display="flex" backgroundColor={colors.primary[400]} borderRadius="3px">
+    <Box display="flex" justifyContent="space-between" p={2}>
+      {/* SEARCH BAR */}
+      <Box
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
+      >
         <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
-          <IconButton type='button' sx={{ p: 1 }}>
-              <Search />
-          </IconButton>
+        <IconButton type="button" sx={{ p: 1 }}>
+          <Search />
+        </IconButton>
       </Box>
+
+      {/* ICONS */}
       <Box display="flex">
         <IconButton onClick={colorMode.toggleColorMode}>
-          {theme.palette.mode === 'dark' ?  (
+          {theme.palette.mode === "dark" ? (
             <DarkModeOutlined />
           ) : (
             <LightModeOutlined />
@@ -29,14 +43,14 @@ function Topbar() {
           <NotificationsOutlined />
         </IconButton>
         <IconButton>
-        
+          <SettingsOutlined />
         </IconButton>
         <IconButton>
-        
+          <PersonOutlined />
         </IconButton>
       </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default Topbar
+export default Topbar;
